66e51bb7180e60ee7738cdb594878400aee1541d
travis skyles <travis.skyles@gmail.com>
2020-03-16 16:17:30 -0700

    basic login complete

--- can-u-front-end/src/components/login/login.js
+++ can-u-front-end/src/components/login/login.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Else, If, Then } from '../conditionals/conditionals';
 
 
 const Login = props => {
 
   return (
     <View>
-      <SignIn />
-      <SignUp />
+      <If condition={props.signUp === true}>
+        <Then>
+          <SignUp />
+        </Then>
+        <Else>
+          <SignIn />
+        </Else>
+      </If>
     </View>
   )
 }
+
+export default Login;
