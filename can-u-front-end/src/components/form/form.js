fa2ab9082c867c87d4a5d34f648a675063253b9d
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-03-17 10:56:45 -0700

    adjusted posting job actions

--- can-u-front-end/src/components/form/form.js
+++ can-u-front-end/src/components/form/form.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import useForm from '../../hooks/'
 
 
 const TodoForm = props => {
-  
+  const { handleChange, handleSubmit } = useForm(props.handleSubmit);
 
   return (
     <>
       <h3>Add Job</h3>
-      <form onSubmit={props.handleSubmit}>
+      <form onChange={handleChange} onSubmit={handleSubmit}>
         <label>
           <span>Job</span>
           <Field
-            name="text"
+            name="name"
             component="input"
             placeholder="Add Job"
           />
@@ -20,10 +21,15 @@ const TodoForm = props => {
         <label>
           <span>Price</span>
           <Field
+            name="price"
             type="number"
             component="input"
             placeholder="Price"
           />
+        </label>
+        <label>
+          <span>Token Test</span>
+          <Field component="input" type="text" name="token"/>
         </label>
         <button>Add Job</button>
       </form>
@@ -32,5 +38,5 @@ const TodoForm = props => {
 };
 
 export default reduxForm({
-  form: "job"
+  form: "jobs"
 })(TodoForm);
