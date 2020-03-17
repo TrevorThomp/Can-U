1c2b2535492053769b756596e9862556718b64c1
natalielyn <nlmaxwell@gmail.com>
2020-03-16 17:20:47 -0700

    created header component

---
+++ can-u-front-end/src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./header.scss";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1 id="header">Can-U</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
