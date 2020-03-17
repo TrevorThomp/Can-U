824fcf63c28cda5e1b73d1ba90348003790ea95f
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-03-17 09:47:58 -0700

    added callAPI hook

---
+++ can-u-front-end/src/lib/api.js
@@ -0,0 +1,18 @@
+const callAPI = (url, method = "get", body, token, handler, errorHandler) => {
+  return fetch(url, {
+    method: method,
+    mode: "cors",
+    cache: "no-cache",
+    headers: { 
+      "Content-Type": "application/json",
+       "Authorization": "Bearer " + token},
+    body: body ? JSON.stringify(body) : undefined
+  })
+    .then(response => response.json())
+    .then(data => (typeof handler === "function" ? handler(data) : data))
+    .catch(e =>
+      typeof errorHandler === "function" ? errorHandler(e) : console.error(e)
+    );
+};
+
+export { callAPI };
