590cd943dd9ffef3a773ae3582cb41494835d6cd
David Vloedman <djvloedman@gmail.com>
2020-03-20 14:14:12 -0700

    Fix the icons for splash

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Login from '../login/login';
 import './splash.scss';
+import auction from './assets/001-auction.png';
+import bid from './assets/002-bid.png';
+import spyglass from './assets/004-spyglass.png';
 
 /**
  * Component that renders a splash page that holds a login form and is shown if a users lands on the page without being logged in
@@ -13,9 +16,9 @@ const Splash = props => {
       <h1>Welcome to Can-U</h1>
       <p id="p">Do you need help moving? mowing your lawn? setting up ikea furniture? There are not a lot of options to find handy help. Search for handy people across the nation!</p>
       <ul>
-        <li><img src="./assets/001-auction.png" /> Create a job, and let people bid to do that job for you!</li>
-        <li><img src="./assets/002-bid.png" />Whoever bids the lowest amount of money to do that job, wins!</li>
-        <li><img src="./assets/004-spyglass.png" />Also look for jobs that you can do!</li>
+        <li><img src={auction}/> Create a job, and let people bid to do that job for you!</li>
+        <li><img src={bid}/>Whoever bids the lowest amount of money to do that job, wins!</li>
+        <li><img src={spyglass} />Also look for jobs that you can do!</li>
       </ul>
       <Login />
     </div>
