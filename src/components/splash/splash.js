2549bcc59d4a2409dacd7b3f6decd098081ff1fe
natalielyn <nlmaxwell@gmail.com>
2020-03-20 13:54:18 -0700

    spruced up splash page

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -11,7 +11,12 @@ const Splash = props => {
   return (
     <div className="splashDiv">
       <h1>Welcome to Can-U</h1>
-      <p>Do you need help moving? mowing your lawn? setting up ikea furniture? There are not a lot of options to find handy help. Search for handy people across the nation!</p>
+      <p id="p">Do you need help moving? mowing your lawn? setting up ikea furniture? There are not a lot of options to find handy help. Search for handy people across the nation!</p>
+      <ul>
+        <li><img src="./assets/001-auction.png" /> Create a job, and let people bid to do that job for you!</li>
+        <li><img src="./assets/002-bid.png" />Whoever bids the lowest amount of money to do that job, wins!</li>
+        <li><img src="./assets/004-spyglass.png" />Also look for jobs that you can do!</li>
+      </ul>
       <Login />
     </div>
   )
