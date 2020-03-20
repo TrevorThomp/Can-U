0260fc7506a79a3d798da498e79eef478dd80103
david-vloedman <runner@fv-az83.ufb2lstgjhxupgopvh0ulz2cab.bx.internal.cloudapp.net>
2020-03-20 15:20:25 +0000

    Updates

---
+++ precache-manifest.3a4ae5f6141bff94ae5c2b60c97dd6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d26c40a97fdd718d4fef6a00aa3ac66",
+    "url": "/can-u-job-front-end/index.html"
+  },
+  {
+    "revision": "4ea63dc1e2bb35fffd6b",
+    "url": "/can-u-job-front-end/static/css/main.e447d5ed.chunk.css"
+  },
+  {
+    "revision": "afc4818533019ba74bfd",
+    "url": "/can-u-job-front-end/static/js/2.bffe9a24.chunk.js"
+  },
+  {
+    "revision": "da8c8c362b99d7f8d0b5b52e9b1e8a25",
+    "url": "/can-u-job-front-end/static/js/2.bffe9a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea63dc1e2bb35fffd6b",
+    "url": "/can-u-job-front-end/static/js/main.5c4b1edd.chunk.js"
+  },
+  {
+    "revision": "dce10f42842171704b10",
+    "url": "/can-u-job-front-end/static/js/runtime-main.e363f43a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/can-u-job-front-end/precache-manifest.53890d4185d2517276c2b3d6e005fa3c.js"
+  "/can-u-job-front-end/precache-manifest.3a4ae5f6141bff94ae5c2b60c97dd6aa.js"
 );
 
 self.addEventListener('message', (event) => {
