bd10ac526f06987ce3bd597021a09d52d75980a9
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-03-30 14:17:44 -0700

    added alt to images missing

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -15,9 +15,9 @@ const Splash = props => {
     <div className="splashDiv">
       <p id="p">The premiere site for posting jobs you need done, and for finding jobs you would like to work on!</p>
       <ul>
-        <li><img src={auction}/> Create a job, and let people bid to do that job for you!</li>
-        <li><img src={bid}/>Whoever bids the lowest amount of money to do that job, wins!</li>
-        <li><img src={spyglass} />Also look for jobs that you can do!</li>
+        <li><img src={auction} alt="Small computer screen"/> Create a job, and let people bid to do that job for you!</li>
+        <li><img src={bid} alt="Small hand holding object"/>Whoever bids the lowest amount of money to do that job, wins!</li>
+        <li><img src={spyglass} alt="Microscope"/>Also look for jobs that you can do!</li>
       </ul>
       <Login />
     </div>
